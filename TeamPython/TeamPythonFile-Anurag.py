da9414a2ea12c3317a2bddfe091ac07bbfa6ebf9
Anurag Numboori <anumboori@hotmail.com>
2019-10-15 13:14:48 -0400

    new file:   TeamPythonFile-Anurag.py

---
+++ TeamPython/TeamPythonFile-Anurag.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 11:47:26 2019
+
+@author: anumb
+"""
+#asks the user for input files 
+input_file = input('Enter the input file with .txt attached: ')
+output_file = input('Enter the output file with .txt attached: ')
+#asks the user for input process
+user_answer = input("whatchu wanna do?: ")
+
+
+#sets ups
+if user_answer = 'Rotate':
+    user_answer = input('Do you want to rotate 90/180/270 or Mirror?: ')
+    if user_answer = 'Mirror':
+#        read, run function for mirror, output file
+        pass
+elif user_answer = '
+        
